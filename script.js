6487a74c9aaf7f1debaa9fb0d6ecc8c8353e2952
git-user-ui <pratikpp2302@gmail.com>
2025-12-03 13:38:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+const age = document.getElementById("age");
+const myname = document.getElementById("name");
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let inputAge = age.value.trim();
+  let inputName = myname.value.trim();
+
+  // Validation
+  if (inputAge === "" || inputName === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  // Promise Logic
+  promiseFunc(inputAge, inputName)
+    .then((msg) => alert(msg))
+    .catch((err) => alert(err));
+}
+
+function promiseFunc(age, name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+}
+
